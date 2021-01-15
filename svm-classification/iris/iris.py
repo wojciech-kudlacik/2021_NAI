48d7779bf05ffaebac7138f4c4228b9e5cf91737
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2021-01-15 18:13:55 +0100

    refactored iris once again

--- svm-classification/iris/iris.py
+++ svm-classification/iris/iris.py
@@ -5,13 +5,16 @@ This program classifies data from the Iris Dataset.
 Type of an Iris flower is being determined by the length and width of its petals and sepals.
 Link: https://archive.ics.uci.edu/ml/machine-learning-databases/iris/
 It utilizes sklearn library as the primary engine to perform classification.
+Tutorials used:
 """
 
 from iris_setup import IrisSetup
 import pandas as pd
 import matplotlib.pylab as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.svm import SVC
 from typing import List
 
@@ -27,17 +30,45 @@ class IrisClassification:
         self.sepal_data = self.iris_dataset.iloc[:, :2]
         self.petal_data = self.iris_dataset.iloc[:, 2:]
 
+
+
     @staticmethod
-    def get_user_input() -> List[str]:
+    def get_user_input() -> dict:
         """
-        Method to get input from User
-        :return: List of user inputs, that describe Iris flower
+        Method to get input from a user to adjust classification parameters
+        :return: Dictionary of parameters to adjust the algorithm
         """
-        sepal_len = input("Sepal Len [cm]: ")
-        sepal_width = input("Sepal Width [cm]: ")
-        petal_len = input("Petal Len [cm]: ")
-        petal_width = input("Sepal Width [cm]: ")
-        return [sepal_len, sepal_width, petal_len, petal_width]
+        test_size = input("Enter test size value (0.25 default): ")
+        if test_size is "":
+            test_size = 0.25
+            print(test_size)
+
+        tts_rs = input("Enter random state seed value for tts method: ")
+        if tts_rs is "":
+            tts_rs = np.random.randint(100)
+            print(tts_rs)
+        else:
+            tts_rs = int(tts_rs)
+
+        ker_rs = input("Enter random state seed value for kernel: ")
+        if ker_rs is "":
+            ker_rs = np.random.randint(100)
+            print(ker_rs)
+        else:
+            ker_rs = int(ker_rs)
+
+        cv = input("Enter number of folds value (default 5): ")
+        if cv is "":
+            cv = "5"
+
+        inputs = {
+            "test_size": test_size,
+            "tts_rs": tts_rs,
+            "ker_rs": ker_rs,
+            "cv": cv
+        }
+
+        return inputs
 
     def visualize_data(self, iris_part: str) -> None:
         """
@@ -72,39 +103,87 @@ class IrisClassification:
         plt.title('Correlation on iris classes')
         plt.show()
 
-    def train(self) -> SVC:
+    def classify_data(self, test_size: int, tts_rs: int, ker_rs: int, cv: int) -> None:
         """
-        Method to train the classifier based on the provided dataset
-        :return: classifier: SVC model
+        Method to predict the outcome of classification. Dataset is split into test data and train subsets.
+        Method prints out the classification report, accuracy of the training and standard deviation.
+        :param test_size: How much of the dataset should be split as test data
+        :param tts_rs: Seed of the data randomizer (by default it is a np.rand value)
+        :param ker_rs: Seed of the kernel randomizer (by default it is a np.rand value)
+        :param cv: Determines the cross-validation splitting strategy. Possible inputs for cv are:
+                - None, to use the default 5-fold cross validation, - int, to specify the number of folds
         """
-        x_train, x_test, y_train, y_test = train_test_split(self.iris_measurements, self.iris_target, test_size=0.25)
-        classifier = SVC(kernel='linear')
+        """
+        train_test_split is a function in Sklearn model selection for splitting data arrays into two subsets: 
+        for training data and for testing data. With this function, you don't need to divide the dataset manually.
+        By default, Sklearn train_test_split will make random partitions for the two subsets. 
+        However, you can also specify a random state for the operation.
+        """
+        x_train, x_test, y_train, y_test = train_test_split(self.iris_dataset.iloc[:, :-1], self.iris_target,
+                                                            test_size=float(test_size), random_state=tts_rs)
+        # linear kernel is used to classify data
+        classifier = SVC(kernel='linear', random_state=ker_rs)
+        # fit the SVM model according to the given training data.
         classifier.fit(x_train, y_train)
-        return classifier
-
-    @staticmethod
-    def make_prediction(classifier, measurements) -> str:
-        """
-        Makes a prediction based on the previously learnt data and provided input from a user
-        :param classifier: SVC model
-        :param measurements: User input that describes Iris flower
-        :return: Predicted Iris Flower class
-        """
-        return classifier.predict([measurements])
+        # perform classification on X, X being all the data from the dataset, without the type of Iris
+        y_pred = classifier.predict(x_test)
+        accuracies = cross_val_score(classifier, x_train, y_train, cv=int(cv))
+        print(classification_report(y_test, y_pred))
+        # {:.2f} is used to reduce the number of decimal points to 2
+        print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+        print("Std Deviation: {:.2f} %".format(accuracies.std()*100))
 
     def run(self) -> None:
         """
         Method to run the program
         """
-        self.visualize_data("Sepal")
-        self.visualize_data("Petal")
-        self.visualize_heatmap()
+        # self.visualize_data("Sepal")
+        # self.visualize_data("Petal")
+        # self.visualize_heatmap()
         inputs = self.get_user_input()
-        classifier = self.train()
-        result = self.make_prediction(classifier, inputs)
-        print(result)
+        test_size = inputs.get("test_size")
+        tts_rs = inputs.get("tts_rs")
+        ker_rs = inputs.get("ker_ts")
+        cv = inputs.get("cv")
+
+        self.classify_data(test_size, tts_rs, ker_rs, cv)
 
 
 if __name__ == '__main__':
     dataset = IrisSetup().create_dataset_from_file()
     IrisClassification(dataset).run()
+
+    # Methods to use if you want to get results from User input
+    # @staticmethod
+    # def get_user_input() -> List[str]:
+    #     """
+    #     Method to get input from User
+    #     :return: List of user inputs, that describe Iris flower
+    #     """
+    #     sepal_len = input("Sepal Len [cm]: ")
+    #     sepal_width = input("Sepal Width [cm]: ")
+    #     petal_len = input("Petal Len [cm]: ")
+    #     petal_width = input("Sepal Width [cm]: ")
+    #     return [sepal_len, sepal_width, petal_len, petal_width]
+
+    #
+    # def train(self) -> SVC:
+    #     """
+    #     Method to train the classifier based on the provided dataset
+    #     :return: classifier: SVC model
+    #     """
+    #     x_train, x_test, y_train, y_test = train_test_split(self.iris_measurements, self.iris_target, test_size=0.25)
+    #     classifier = SVC(kernel='linear')
+    #     classifier.fit(x_train, y_train)
+    #     return classifier
+    #
+    # @staticmethod
+    # def make_prediction(classifier, measurements) -> str:
+    #     """
+    #     Makes a prediction based on the previously learnt data and provided input from a user
+    #     :param classifier: SVC model
+    #     :param measurements: User input that describes Iris flower
+    #     :return: Predicted Iris Flower class
+    #     """
+    #     return classifier.predict([measurements])
+
