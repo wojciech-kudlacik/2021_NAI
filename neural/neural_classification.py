2a86d2b7b29f299b3bf97473f6278ed09931fbc0
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2021-01-27 17:07:48 +0100

    refactored neural classifier

--- neural/neural_classification.py
+++ neural/neural_classification.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class NeuralClassification:
@@ -19,7 +20,8 @@ class NeuralClassification:
 
             new_model = input('Train new model? (y/n): ')
             if new_model == 'y':
-                model = self.train(x_train, y_train, x_test, y_test)
+                epochs = input('Number of epochs?: ')
+                model = self.train(x_train, y_train, x_test, y_test, epochs, dataset_number)
 
                 save_model = input('Save model? (input name if you want to save it): ')
                 if save_model != "":
@@ -41,56 +43,74 @@ class NeuralClassification:
     def unpack_dataset(dataset):
         return dataset.load_data()
 
-    def train(self, x_train, y_train, x_test, y_test):
+    def train(self, x_train, y_train, x_test, y_test, epochs, dataset_number):
 
         normalized_x_train = tf.keras.utils.normalize(x_train, axis=1)
         normalized_x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-        model = Sequential()
-        # model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',
-        #                  input_shape=(32, 32, 1)))
-        # model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-        # model.add(MaxPooling2D((2, 2)))
-        model.add(Flatten())
-        model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-        model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-        model.add(Dense(10, activation='softmax'))
+        layers = {'1': self.iris_layers() ,'2': self.cifar_layers(), '3': self.fashion_layers(), '4': self.heritage_layers()}
+        model = layers.get(dataset_number)
 
         model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-        model.fit(normalized_x_train, y_train, epochs=1)
+        model.fit(normalized_x_train, y_train, epochs=int(epochs))
 
         self.evaluate_model(model, normalized_x_test, y_test)
 
         return [model, normalized_x_test, y_test]
 
+    @staticmethod
+    def cifar_layers():
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',
+                         input_shape=(32, 32, 3)))
+        model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(10, activation='softmax'))
+
+        return model
+
+    @staticmethod
+    def fashion_layers():
+        model = Sequential()
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(10, activation='softmax'))
+
+        return model
+
+    def heritage_layers(self):
+        pass
+
+    def iris_layers(self):
+        pass
+
     def choose_dataset(self, dataset_number):
         dataset = None
         if dataset_number == 1:
             print('iris dataset')
-            self.dataset_name = 'Iris'
+            self.categories = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+            # load external dataset
         elif dataset_number == 2:
             print('CIFAR 10')
+            self.categories = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
             dataset = tf.keras.datasets.cifar10
-            self.dataset_name = 'cifar'
         elif dataset_number == 3:
             print('Fashion MNIST')
+            self.categories = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat','Sandal', 'Shirt', 'Sneaker', 'Bag',
+                'Ankle boot']
             dataset = tf.keras.datasets.fashion_mnist
-            self.dataset_name = 'fashion'
         elif dataset_number == 4:
-            print('MNIST')
-            dataset = tf.keras.datasets.reuters
+            print('Architectural Heritage')
+            self.categories = ['Altar', 'Apse', 'Bell tower', 'Column', 'Dome (inner)', 'Dome (outer)', 'Flying buttress',
+                               'Gargoyle (and Chimera)', 'Stained glass', 'Vault']
+            # load external dataset
 
         return dataset
-
-    def choose_categories(self):
-        if self.dataset_name == 'cifar':
-            self.categories = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-        elif self.dataset_name == 'fashion':
-            self.categories = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-        elif self.dataset_name == ''
-
 
     @staticmethod
     def evaluate_model(model, x_test, y_test):
@@ -104,17 +124,18 @@ class NeuralClassification:
         print('Saved')
 
     def predict_from_dataset(self, model, x_test, y_test):
-        self.choose_categories()
-        print(self.categories)
-        model.predict([x_test])
-        print(y_test[123])
+        prediction = model.predict([x_test])
+        predicted_category = np.argmax(prediction[123])
+        print(predicted_category)
+        print('Predicted category: ' + self.categories[int(predicted_category)])
+
         plt.imshow(x_test[123])
-        print(self.categories[int(y_test[123][0])])
+        print('Actual category: ' + self.categories[int(y_test[123])])
         plt.show()
 
     @staticmethod
     def read_model_from_file(path):
-        return tf.keras.models.load_model(path)
+        return tf.keras.models.load_model(path + '.model')
 
     def run(self):
         self.get_user_input()
